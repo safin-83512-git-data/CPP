6c8c97ae122e95845ecc534bd5f3abe199c2882e
kd2-safin-83512 <shafintamboli007@gmail.com>
2024-03-20 18:46:12 +0530

    Assignment 2 Updated

--- ASSIGN2_CPP_KD2-SAFIN/Assign2-Q2.cpp
+++ ASSIGN2_CPP_KD2-SAFIN/Assign2-Q2.cpp
@@ -5,32 +5,39 @@ using namespace std;
 class TollBooth {
 private:
     unsigned int totalCars;
+    unsigned int payingCars;
+    unsigned int nonPayingCars;
     double totalMoney;
 
 public:
-    TollBooth() : totalCars(0), totalMoney(0.0) {}
-
-    void payingCar() {
-        totalCars++;
-        totalMoney += 0.50;
-    }
-
-    void nopayCar() {
-        totalCars++;
+    TollBooth() : totalCars(0), payingCars(0), nonPayingCars(0), totalMoney(0.0) {}
+    
+    void addCars(unsigned int paying, unsigned int nonPaying) {
+        totalCars += paying + nonPaying;
+        payingCars += paying;
+        nonPayingCars += nonPaying;
+        totalMoney += (0.50 * paying);
     }
 
     void printOnConsole() {
         cout << "Total Cars: " << totalCars << endl;
         cout << "Total Money Collected: $" << totalMoney << endl;
-        cout << "Number of Paying Cars: " << totalCars << endl;
-        cout << "Number of Non-Paying Cars: " << totalCars << endl;
+        cout << "Number of Paying Cars: " << payingCars << endl;
+        cout << "Number of Non-Paying Cars: " << nonPayingCars << endl; 
     }
 };
 
 int main() {
     TollBooth booth;
-    booth.payingCar(); 
-    booth.nopayCar(); 
+    unsigned int paying, nonPaying;
+
+    cout << "Enter the number of paying cars: ";
+    cin >> paying;
+    cout << "Enter the number of non-paying cars: ";
+    cin >> nonPaying;
+
+    booth.addCars(paying, nonPaying);
+
     booth.printOnConsole();
 
     return 0;
